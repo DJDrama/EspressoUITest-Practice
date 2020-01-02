21728d024a1555b179b5e444f0510ecf9317f9d5
DJ <putzzang@gmail.com>
2020-01-02 10:58:09 +0900

    Testing Activities in Isolation

---
+++ app/src/main/java/com/djdj/espressotest/SecondaryActivity.kt
@@ -0,0 +1,17 @@
+package com.djdj.espressotest
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_secondary.*
+
+class SecondaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary)
+
+    }
+
+}
