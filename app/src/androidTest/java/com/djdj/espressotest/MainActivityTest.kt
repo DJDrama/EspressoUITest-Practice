d5f744a3168d5a5c5ea68038316ef7d8086cd955
DJ <putzzang@gmail.com>
2020-01-06 14:56:21 +0900

    Gallery Intent and startActivityForResult

--- app/src/androidTest/java/com/djdj/espressotest/MainActivityTest.kt
+++ app/src/androidTest/java/com/djdj/espressotest/MainActivityTest.kt
@@ -1,13 +1,30 @@
 package com.djdj.espressotest
 
 import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.app.Instrumentation
+import android.content.ContentResolver
+import android.content.Intent
+import android.content.res.Resources
+import android.net.Uri
+import android.provider.MediaStore
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry
+import com.djdj.espressotest.ui.MainActivity
+import org.hamcrest.Matcher
+import org.hamcrest.core.AllOf.allOf
+import org.junit.Rule
 import org.junit.Test
 
 import org.junit.runner.RunWith
@@ -55,4 +72,37 @@ class MainActivityTest {
 //        pressBack() //method2
 //        onView(withId(R.id.main)).check(matches(isDisplayed()))
 //    }
+
+    @get: Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
+
+    @Test
+    fun test_validateIntentSentToPickPackage() {
+        //GIVEN
+        val expectedIntent: Matcher<Intent> = allOf(
+            hasAction(Intent.ACTION_PICK), //action
+            hasData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI) //datatype
+        )
+        val activityResult = createGalleryPickActivityResultStub()
+        intending(expectedIntent).respondWith(activityResult)
+
+        // Execute and Verify
+        onView(withId(R.id.button_open_gallery)).perform(click())
+        intended(expectedIntent)
+
+    }
+
+    private fun createGalleryPickActivityResultStub(): Instrumentation.ActivityResult {
+        val resource: Resources = InstrumentationRegistry.getInstrumentation()
+            .context.resources
+        val imageUri = Uri.parse(
+            ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
+                    resource.getResourcePackageName(R.drawable.ic_launcher_background) + "/" +
+                    resource.getResourceTypeName(R.drawable.ic_launcher_background) + "/" +
+                    resource.getResourceEntryName(R.drawable.ic_launcher_background)
+        )
+        val resultIntent = Intent()
+        resultIntent.data = imageUri
+        return Instrumentation.ActivityResult(RESULT_OK, resultIntent)
+    }
 }
